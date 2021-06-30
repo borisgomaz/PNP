803d52445d4ebcfc9cacb8ce0452ba219c5d8dcf
Boris <boris.gomaz@gmail.com>
2021-06-30 10:00:31 +0200

    Added tsv files to .gitignore.

--- koordinate.py
+++ koordinate.py
@@ -19,41 +19,36 @@ for n in f.readlines():
 parser = PDB.PDBParser()
 pdb = '../4tta.pdb'
 structure = parser.get_structure('4tta', pdb)
+model = structure[0]
 for res in result:
-    atom1 = structure[0][res.get('chainID1')][int(res.get('AAID1'))][res.get('AAatom1')] 
-    atom2 = structure[0][res.get('chainID2')][int(res.get('AAID2'))][res.get('AAatom2')]
-        
-    if 'water2' in res.keys():
-        atom_w1 = structure[0][res.get('chainIDw')]['W', int(res.get('waterID')), ' '][res.get('water_atom')]
-        atom_w2 = structure[0][res.get('chainIDw2')]['W', int(res.get('waterID2')), ' '][res.get('water_atom2')]
-          
-        distance_a1w1 = atom1 - atom_w1
-        distance_w1w2 = atom_w1 - atom_w2
-        distance_w2a2 = atom_w2 - atom2
-        res.update({'distance_atom1_water1': distance_a1w1})
-        res.update({'distance_water1_water2': distance_w1w2})
-        res.update({'distance_water2_atom2': distance_w2a2})
+    chain1 = model[res.get('chainID1')]
+    chain2 = model[res.get('chainID2')]
+    residue1 = chain1[int(res.get('AAID1'))] 
+    residue2 = chain2[int(res.get('AAID2'))]
+    atom1 = residue1[res.get('AAatom1')] 
+    atom2 = residue2[res.get('AAatom2')]
     
-    elif 'water' in res.keys():
-        atom_w = structure[0][res.get('chainIDw')]['W', int(res.get('waterID')), ' '][res.get('water_atom')]
-      
-        distance_aw = atom1 - atom_w
-        distance_wa = atom_w - atom2
+    distance = atom1 - atom2
+    res.update({'distance': distance})
+    
+    if 'water' in res.keys():
+        chain3 = model[res.get('chainIDw')]
+        residue3 = chain3['W', int(res.get('waterID')), ' ']
+        atom3 = residue3[res.get('water_atom')]
+        
+        distance_aw = atom1 - atom3
+        distance_wa = atom3 - atom2
         res.update({'distance_atom1_water': distance_aw})
         res.update({'distance_water_atom2': distance_wa})
-    
-    else:
-        distance = atom1 - atom2
-        res.update({'distance': distance})
-
+        
 #Types of bonds: ["hp", "sb", "pc", "ps", "ts", "vdw", "hb", "wb", "wb2"]
 #For hb bonds: if m['bond_type'][:2] == 'hb':
 short = []
 for m in result:
-    if m['bond_type'] == 'sb':
+    if m['bond_type'] == 'wb':
         short.append(m)
 df = pd.DataFrame(short)
 #For maximal rows dislay:
 #pd.set_option('display.max_rows', df.shape[0]+1)
 print(df)        
-        
+    
